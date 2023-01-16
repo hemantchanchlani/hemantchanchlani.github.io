68698e362d172008eccf21a0977331e52366b6bd
hemantchanchlani <hemantchanchlani@gmail.com>
2023-01-15 20:51:59 -0600

    Change in PCR graph

--- oi.js
+++ oi.js
@@ -3,8 +3,9 @@ var puts = [];
 var openInterestChart = null;
 var volumeChart = null;
 var pcrChart = null;
+var lastPcrChart = null;
 
-var strikeRange = 400;
+var strikeRange = 200;
 var openToday = 4000;
 
 var callOI = {};
@@ -12,6 +13,8 @@ var putOI = {};
 var supportNumbers = [];
 var resistancetNumbers = [];
 var maxOpenInterest = 50;
+var lastPCRs = [];
+var maxOpenInterest = 250;
 
 var saveInLocal = true;
 
@@ -108,7 +111,7 @@ function prepareChart() {
 							return value.volume;
 						},
 						formatter: function(value, context) {
-							return value.strike + "(" + value.open_interest + ")";
+							return value.strike + "(" + value.volume + ")";
 						},
 						color: 'black',
 						font: {
@@ -127,6 +130,54 @@ function prepareChart() {
 
 
 
+	lastPcrChart = new Chart(
+		document.getElementById('lastPCR'),
+		{
+			type: 'line',
+			plugins: [ChartDataLabels],
+			data: {
+
+				datasets: [{
+					label: 'PCR Ratio',
+					data: lastPCRs
+				}]
+			},
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true,
+					}
+				}]
+			},
+			options: {
+				interaction: {
+					intersect: false,
+					mode: 'index',
+				},
+				plugins: {
+					datalabels: {
+						anchor: 'end',
+						align: 'top',
+						formatter: function(value, context) {
+							return value.ratio;
+						},
+						color: 'black',
+						font: {
+							size: '10'
+						}
+					}
+				},
+				parsing: {
+					xAxisKey: 'time',
+					yAxisKey: 'ratio',
+				}
+			}
+		}
+	);
+
+
+
+
 	pcrChart = new Chart(
 		document.getElementById('PCR'),
 		{
@@ -135,10 +186,10 @@ function prepareChart() {
 			data: {
 
 				datasets: [{
-					label: 'SUPPORT',
+					label: 'SUPPORT (Puts / Calls)',
 					data: supportNumbers
 				}, {
-					label: 'RESISTANCE',
+					label: 'RESISTANCE (Calls / Puts) ',
 					data: resistancetNumbers
 				}]
 			},
@@ -218,7 +269,6 @@ function getQuotes(time, fileName) {
 
 		}
 
-
 	}
 
 
@@ -236,11 +286,11 @@ function getQuotes(time, fileName) {
 			'Accept': 'application/json'
 		},
 		success: function(data, response, code) {
-			openToday = data.quotes.quote.last;
+			openToday = data.quotes.quote.open;
 			getOpenInt(symbol, dateOfExpiry);
 			setInterval(function() {
 				getOpenInt(symbol, dateOfExpiry);
-			}, 2000000);
+			}, 1 * 60 * 1000);
 
 		}
 	}
@@ -277,47 +327,58 @@ function getOpenInt(symbol, dateOfExpiry) {
 			volumeChart.destroy();
 			openInterestChart.destroy();
 			pcrChart.destroy();
-
-
-
+			lastPcrChart.destroy();
 			var options = data.options.option;
 
-
+			var sumofPuts = 0;
+			var sumOfCalls = 0;
 
 
 			options.forEach(function(option, i) {
-
 				if (Math.abs(option.strike - openToday) <= strikeRange) {
 					option.strikeLabel = option.strike + '';
 
 					if (option.symbol.slice(6).indexOf('P') > -1) {
 						puts.push(option);
+						sumofPuts += option.open_interest;
 						putOI[option.strikeLabel] = option.open_interest;
 					}
 
 
 					if (option.symbol.slice(6).indexOf('C') > -1) {
 						calls.push(option);
+						sumOfCalls += option.open_interest;
 						if (option.open_interest > maxOpenInterest) {
 							callOI[option.strikeLabel] = option.open_interest;
 						}
 					}
 				}
-
-
-
 			});
+
+			var timeOfData = new Date().toLocaleString('en-US', {
+				hour12: false,
+			});
+
+			var key = symbol + '-' + dateOfExpiry + '-' + timeOfData;
+
+			var keyforPCR = symbol + '-' + dateOfExpiry + '-pcr';
+
+
+			var previous = localStorage.getItem(keyforPCR);
+			if (previous) {
+				lastPCRs = JSON.parse(previous);
+			}
+			var item = {};
+			item.time = timeOfData;
+			item.ratio = (sumofPuts / sumOfCalls).toFixed(2);
+			lastPCRs.push(item);
+
+			localStorage.setItem(keyforPCR, JSON.stringify(lastPCRs));
 
 
 			if (saveInLocal) {
-
-				var now = new Date();
-
-				var key = symbol + '-' + dateOfExpiry + '-' + new Date().toLocaleTimeString();
-
 				localStorage.setItem(key + '-calls', JSON.stringify(calls));
 				localStorage.setItem(key + '-puts', JSON.stringify(puts));
-
 				saveInLocal = false;
 			}
 
@@ -328,16 +389,12 @@ function getOpenInt(symbol, dateOfExpiry) {
 					var oneVal = {};
 
 					oneVal.strikeLabel = strikeLabel;
-					oneVal.supportOrResistance = Math.round(putOI[strikeLabel] / callOI[strikeLabel] * 10) / 10;
-
+					oneVal.supportOrResistance = Math.round(putOI[strikeLabel] / callOI[strikeLabel] * 100) / 100;
 					supportNumbers.push(oneVal);
 
-
-
 					var secondVal = {};
-
 					secondVal.strikeLabel = strikeLabel;
-					secondVal.supportOrResistance = Math.round(callOI[strikeLabel] / putOI[strikeLabel] * 10) / 10;;
+					secondVal.supportOrResistance = Math.round(callOI[strikeLabel] / putOI[strikeLabel] * 100) / 100;;
 					resistancetNumbers.push(secondVal);
 
 				}
